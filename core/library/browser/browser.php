01c551c3450500b25996142945620714c6f7f409
Endre Nagy <nagyendre@reea.net>
2012-09-05 17:29:18 +0200

    Separate public part with an abstract class

--- core/library/browser/browser.php
+++ core/library/browser/browser.php
@@ -2,8 +2,57 @@
 
 namespace core\library\browser;
 
+/**
+ * This Browser class is the public part for a Browser management
+ */
+class Browser extends AbstractBrowser {
 
-class Browser {
+# These methods are for initialize a browser page request
+    
+    public function setUA($ua) {
+        parent::setUA($ua);
+    }
+    
+    public function attachCookie($cookie) {
+        parent::attachCookie($cookie);
+    }
+    
+    public function attachPostData($postData) {
+        parent::attachPostData($postData);
+    }
+    
+    public function attachFiles($files) {
+        parent::attachFiles($files);
+    }
+    
+    public function setHeaders($headers) {
+        parent::setHeaders($headers);
+    }
+    
+
+# The load method try to access the url with all the parameters set below
+
+    public function load($url) {
+        parent::load($url);
+    }
+    
+
+#Getter methods will retreive any data returned after fetching the url
+
+    public function getHeaders() {
+        return parent::getHeaders();
+    }
+    
+    public function getResponse() {
+        return parent::getResponse();
+    }
+}
+
+
+
+
+
+abstract class AbstractBrowser {
 
     const USER_AGENT = 'HTTP_USER_AGENT';
 
@@ -45,7 +94,6 @@ class Browser {
     }
     
 
-    
     public function getResponse() {
         
     }
@@ -60,60 +108,4 @@ class Browser {
     }
 }
 
-?>
-
-<?php
-
-
-namespace app\models\ws;
-
-
-class WebserviceCaller {
-
-    /**
-    * @param $xml (string) xml string returned by ws
-    * @return $result (SimpleXmlElement) .
-    */
-    public function xmlToObject($xml){
-        $result = @simplexml_load_string($xml);
-        return $result;
-    }
-
-    /**
-    * @param $params (string) ws url
-    * @param $params (string) parameters to send to ws url encoded
-    * @return $xmlResult (string) xml returned by ws
-    */
-    public function curlCall($url, $params){
-        $cookieFile = "/tmp/CURLCOOKIE".md5(session_id());
-
-        try{
-            $curlHandler = curl_init();
-            curl_setopt($curlHandler, CURLOPT_URL, $url);
-            curl_setopt($curlHandler, CURLOPT_COOKIEJAR, $cookieFile);
-            curl_setopt($curlHandler, CURLOPT_COOKIEFILE, $cookieFile);
-            curl_setopt($curlHandler, CURLOPT_POST, count($params));
-            curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $params);
-            curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, true);
-            // curl_setopt($curlHandler, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:12.0) Gecko/20100101 Firefox/12.0');
-            // curl_setopt($curlHandler, CURLOPT_HTTPHEADER, $header);
-            $result = curl_exec($curlHandler);
-
-            if(empty($result)){
-                $error = "curlCall method throw an exception trying to call: \n".$url."\n with parameters \n".$params;
-                return new \Exception($error, -1);
-            }
-
-            if($result == 'error'){
-                $error = $url.' returned an unknown error.';
-                return new \Exception($error, -2);
-            }
-
-            return $result;
-        }catch(Exception $e){
-            $error = "curlCall method throw an exception trying to call: \n".$url."\n with parameters \n".$params;
-            return new \Exception($error, -3);
-        }
-    }
-}
 ?>
