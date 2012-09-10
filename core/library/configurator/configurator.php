2cfd3361e3befa65247196c69b7d3644471e78a5
Endre Nagy <nagyendre@reea.net>
2012-09-10 16:59:12 +0200

    Add Symphony YML lib into vendor and some other config

---
+++ core/library/configurator/configurator.php
@@ -0,0 +1,13 @@
+<?php
+namespace core\library\configurator;
+
+class Configurator {
+
+    public function __construct($solutioner) {
+        $this->solutioner = $solutioner;
+    }
+    
+    
+}
+
+?>
